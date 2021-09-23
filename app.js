2b191b8c8c2966c14601bb80657cbb2c973c99a0
Vaibhav Vyas <52570863+GenxTheGamer@users.noreply.github.com>
2021-09-23 11:08:00 +0530

    added styling , app fully functional

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashAmount = document.querySelector("#cash-amount");
-const buttonCheck = document.querySelector("#checkButton");
-const Message = document.querySelector(".show-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+const billAmount = document.querySelector("#bill-amount"); //Bill Amount Input
+const cashAmount = document.querySelector("#cash-amount"); //Cash Amount Input
+const buttonCheck = document.querySelector("#checkButton"); //Button for taking the inputs and give output
+const Message = document.querySelector(".show-message"); //Shows Error Message according to billAmount and cashAmount
+const noOfNotes = document.querySelectorAll(".no-of-notes"); //This is the no. of notes row selected by class name all at once.
 
-const Notes = [2000, 500, 100, 20, 10, 5, 1];
+const Notes = [2000, 500, 100, 20, 10, 5, 1]; //Array of Notes
 
 buttonCheck.addEventListener("click", function checkNotesAmount() {
-  Message.style.display = "none";
+  hideMessage(); //To initially hide the Message if there is no error.
   if (billAmount.value > 0) {
     if (cashAmount.value >= billAmount.value) {
-      const billAndCashAmountDifference = cashAmount.value - billAmount.value;
-      calculateChange(billAndCashAmountDifference);
+      const billAndCashAmountDifference = cashAmount.value - billAmount.value; //We Calculate the difference of cash amount & bill amount for further use
+      calculateChange(billAndCashAmountDifference); //Function to calculate the result we need.
     } else {
-      showMessage("Really?");
+      showMessage("Really?"); //If your bill amount is greater then RIP.
     }
   } else {
     showMessage("Invalid amount");
@@ -22,10 +22,15 @@ buttonCheck.addEventListener("click", function checkNotesAmount() {
 
 function calculateChange(billAndCashAmountDifference) {
   for (let i = 0; i < Notes.length; i++) {
-    const numberofNotes = Math.trunc(billAndCashAmountDifference / Notes[i]);
-    billAndCashAmountDifference %= Notes[i];
+    //Loop to iterate at each note to calculate amt of notes we will need.
+    const numberofNotes = Math.trunc(billAndCashAmountDifference / Notes[i]); //Math.trunc is js function for mathematical stuff.
+    billAndCashAmountDifference %= Notes[i]; //Just like we do ++i we are differencing them each time
     noOfNotes[i].innerText = numberofNotes;
   }
+}
+
+function hideMessage() {
+  Message.style.display = "none";
 }
 
 function showMessage(msg) {
